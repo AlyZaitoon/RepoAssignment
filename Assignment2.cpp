d7da42c206a6c53107770afd3b2b0d2e7c6e505a
AlyZaitoon <zaitoonaly4@gmail.com>
2023-02-17 15:11:12 +0200

    Added Min Function

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -12,8 +12,17 @@ int getVectorSum(vector<int>& input){
     return sum;
 }
 
+int getMin(vector<int>& input){
+    int min = input[0];
+    for(int i=1;i<input.size();i++){
+       min=min<input[i]?min:input[i];
+    }
+    return min;
+}
+
 int main(){
     vector<int> test = {1,2,3,4,5,6,7,8,9,10};
     cout << getVectorSum(test)<<endl;
+    cout << getMin(test)<<endl;
     return 0;
 }
